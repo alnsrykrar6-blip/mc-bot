9628453c7d1664efc4eff27657b5dc2f932a3ebb
alnsrykrar6-blip <alnsrykrar6@gmail.com>
2026-03-28 15:28:40 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+const mineflayer = require('mineflayer')
+
+console.log("بدء الاتصال...")
+
+const bot = mineflayer.createBot({
+  host: 'dynamic-8.magmanode.com',
+  port: 25780,
+  username: 'Player' + Math.floor(Math.random() * 1000),
+  version: false,
+  auth: 'offline'
+})
+
+bot.on('login', () => {
+  console.log('تم تسجيل الدخول ✅')
+})
+
+bot.on('spawn', () => {
+  console.log('دخل السيرفر 🔥')
+  
+  // يتحرك حتى ما ينكشف
+  setInterval(() => {
+    bot.setControlState('jump', true)
+    setTimeout(() => bot.setControlState('jump', false), 300)
+  }, 10000)
+})
+
+bot.on('error', (err) => {
+  console.log('خطأ ❌:', err.message)
+})
+
+bot.on('end', () => {
+  console.log('انقطع ❌')
+})
